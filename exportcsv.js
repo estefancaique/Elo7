8477501745b98efbb4c4240cc11e6787345dd6b0
estefancaique <estefancaique@gmail.com>
2018-09-22 09:35:25 -0300

    Removendo Arquivo Desnecessario
    - Removendo Arquivo exportcsv.js o mesmo não foi mais usado depois de ajustes no codigo

--- exportcsv.js
+++
@@ -1,7 +0,0 @@
- app.get('/export', function(req, res)
-    {
-        var spawn = require('child_process').spawn,
-        ls = spawn('mongoexport',['--db','monopoly','--collection','newYork']);
-        res.sendfile('/home/database.csv') 
-
-    });
